c0f004ebd1b0da494d7d43b0b0a1f570642de068
nyolarraklay <nyolosorii@gmail.com>
2023-06-03 02:15:05 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// icon navbar
+
+const menuIcon = document.querySelector("#menu-icon");
+const navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
